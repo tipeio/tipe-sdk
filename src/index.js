3cf9f6cceaeef21581c09f328fdb50d7b453c0a9
PatrickJS <github@gdi2290.com>
2018-05-15 20:18:38 -0700

    feat: source

--- src/index.js
+++ src/index.js
@@ -1,48 +1,41 @@
-const fetch = require('node-fetch');
+import { Client, RestClient, GraphqlClient } from './client'
 
-var API_KEY = process.env.TIPE_API_KEY
-var ORG_KEY = process.env.TIPE_ORG_KEY
+function createClient(config) {
+  config = {
+    type: 'graphql',
+    ...(config || {})
+  }
 
-function tipeFetch (type, id) {
-  return fetch('https://api.tipe.io/api/v1/' + type + '/' + id, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': API_KEY,
-      'Tipe-Id': ORG_KEY,
-    }
-  })
-  .then(function (res) {
-    if (res.status >= 200 && res.status <= 302) {
-      return res.json()
-    } else {
-      if (res.status === 401) {
-        return new Error('Error 401: Make sure you have a valid Org Secret Key and API Key')
-      }
-      return new Error(res.message || 'There was an error making a request')
-    }
-  })
-}
+  const apiKey =
+    config.apiKey ||
+    config.ApiKey ||
+    config.Authorization ||
+    config.authorization ||
+    process.env.TIPE_API_KEY
 
-function createClient (config) {
-  const apiKey = config.apiKey || config.ApiKey || config.Authorization || config.authorization
+  const orgKey =
+    config.orgKey ||
+    config.orgId ||
+    config.OrgKey ||
+    config.tipeId ||
+    config.TipeId ||
+    process.env.TIPE_ORG_KEY
+
   if (apiKey) {
-    API_KEY = apiKey
+    config.apiKey = apiKey
   }
-  const orgKey = config.orgKey || config.orgId || config.OrgKey
   if (orgKey) {
-    ORG_KEY = orgKey
+    config.orgKey = orgKey
   }
-  return {
-    getFolder(id) {
-      return tipeFetch('folder', id)
-    },
-    getDocument(id) {
-      return tipeFetch('document', id)
-    }
-  }
-}
 
+  let client = new Client()
+  if (config.type === 'rest') {
+    client = new RestClient()
+  } else if (config.type === 'graphql') {
+    client = new GraphqlClient()
+  }
+  return client
+}
 
 module.exports = {
   createClient
