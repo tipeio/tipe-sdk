8ebacf2bec51a5c76151b37a2525d24683b8687a
PatrickJS <github@gdi2290.com>
2018-05-16 16:10:01 -0700

    chore: create client

--- src/index.js
+++ src/index.js
@@ -1,42 +1,6 @@
-import { Client, RestClient, GraphqlClient } from './client'
+import create from './create-client'
 
-function createClient(config) {
-  config = {
-    type: 'graphql',
-    ...(config || {})
-  }
-
-  const apiKey =
-    config.apiKey ||
-    config.ApiKey ||
-    config.Authorization ||
-    config.authorization ||
-    process.env.TIPE_API_KEY
-
-  const orgKey =
-    config.orgKey ||
-    config.orgId ||
-    config.OrgKey ||
-    config.tipeId ||
-    config.TipeId ||
-    process.env.TIPE_ORG_KEY
-
-  if (apiKey) {
-    config.apiKey = apiKey
-  }
-  if (orgKey) {
-    config.orgKey = orgKey
-  }
-
-  let client = new Client()
-  if (config.type === 'rest') {
-    client = new RestClient()
-  } else if (config.type === 'graphql') {
-    client = new GraphqlClient()
-  }
-  return client
-}
-
-module.exports = {
+export const createClient = create
+export default {
   createClient
 }
