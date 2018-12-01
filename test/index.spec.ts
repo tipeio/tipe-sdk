9a258854fa4c6ffde0fa8503233c6aec0325417c
PatrickJS <github@gdi2290.com>
2018-11-30 18:32:08 -0800

    test: methods

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,11 +1,22 @@
+import Tipe, { createClient } from '../src'
 jest.mock('node-fetch', () => {
-  return function() {
-    return Promise.resolve()
+  return function(url: string, options: any) {
+    // sync promise
+    const promise = {
+      then(fn: any) {
+        return fn([url, options])
+        return promise
+      },
+      catch(fn?: any) {
+        return promise
+      }
+    }
+    return promise
   }
 })
-
-import Tipe, { createClient } from '../src'
-
+console.log = s => {
+  process.stdout.write(s + "\n")
+}
 describe('Tipe', () => {
   test('should work', () => {
     expect(Tipe).not.toBe(undefined)
@@ -13,5 +24,47 @@ describe('Tipe', () => {
   test('should createClient', () => {
     const tipe = createClient({})
     expect(tipe instanceof Tipe).toBe(true)
+  })
+  test('should request a document', () => {
+    const tipe = createClient({
+      domain: 'yolo.com',
+      project: '090909',
+      key: '8989'
+    })
+    tipe.document({
+      id: '555'
+    }).then(([url, options]) => {
+      expect(url).toEqual('yolo.com/api/090909/sdk')
+      expect(JSON.parse(options.body).type).toEqual('document')
+    })
+    .catch(() => {})
+  })
+  test('should request a asset', () => {
+    const tipe = createClient({
+      domain: 'yolo.com',
+      project: '090909',
+      key: '8989'
+    })
+    tipe.asset({
+      id: '555'
+    }).then(([url, options]) => {
+      expect(url).toEqual('yolo.com/api/090909/sdk')
+      expect(JSON.parse(options.body).type).toEqual('asset')
+    })
+    .catch(() => {})
+  })
+  test('should request a page', () => {
+    const tipe = createClient({
+      domain: 'yolo.com',
+      project: '090909',
+      key: '8989'
+    })
+    tipe.page({
+      id: '555'
+    }).then(([url, options]) => {
+      expect(url).toEqual('yolo.com/api/090909/sdk')
+      expect(JSON.parse(options.body).type).toEqual('page')
+    })
+    .catch(() => {})
   })
 })
